a1be4aa9c8b24d935d99cf99cf1c8a82fe86abd7
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-06-13 00:42:17 +0200

    Aut 12

--- deploaut/src/App.js
+++ deploaut/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editasaaaaamakallaaasss <code>src/App.js</code> and save to reload.
+          Editasaaaaammskskskskssksskakallaaasss <code>src/App.js</code> and
+          save to reload.
         </p>
         <a
           className="App-link"
